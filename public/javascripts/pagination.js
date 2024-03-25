360be60c31649d9eebd03a230ed4a8eebff718f1
Max Lyu <lyupro21@gmail.com>
2024-03-25 23:29:20 +0100

    CHANGED: Cleaned code from logs

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -3,43 +3,34 @@
 // Функция для загрузки глаголов при переходе на другую страницу через пагинацию
 async function loadVerbs(url) {
     try {
-        console.log('Загрузка глаголов с URL:', url);
 
         const response = await fetch(url);
-        console.log('Ответ получен:', response);
 
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const html = await response.text();
-        console.log('HTML получен:', html);
 
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
-        console.log('HTML разобран:', doc);
 
         // Обновляем только содержимое контейнера с глаголами
         const verbListContainer = document.querySelector('.verb-list');
         if (verbListContainer) {
             verbListContainer.innerHTML = doc.querySelector('.verb-list').innerHTML;
-            console.log('Контейнер с глаголами обновлен');
         } else {
-            console.warn('Контейнер с глаголами не найден');
         }
 
         // Обновляем блок пагинации
         const paginationContainer = document.querySelector('.pagination');
         if (paginationContainer) {
             paginationContainer.innerHTML = doc.querySelector('.pagination').innerHTML;
-            console.log('Блок пагинации обновлен');
         } else {
-            console.warn('Блок пагинации не найден');
         }
 
         // Обновляем URL-адрес страницы без перезагрузки
         history.pushState(null, null, url);
-        console.log('URL-адрес страницы обновлен:', url);
 
         // Сбрасываем состояние поля поиска и окна результатов
         const searchInput = document.getElementById('searchInput');
@@ -48,12 +39,9 @@ async function loadVerbs(url) {
             searchInput.value = '';
             searchResults.innerHTML = '';
             searchResults.classList.add('hidden');
-            console.log('Состояние поля поиска и окна результатов сброшено');
         } else {
-            console.warn('Поле поиска или окно результатов не найдены');
         }
     } catch (error) {
-        console.error('Ошибка при загрузке глаголов:', error);
     }
 }
 
