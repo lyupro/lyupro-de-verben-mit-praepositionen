ba8d66e421722f602353b5c8dc58164bfbdc6733
Max Lyu <lyupro21@gmail.com>
2024-03-27 20:10:07 +0100

    ADDED: Encapsulating functionality to database connection at file db.js

--- db.js
+++ db.js
@@ -1,12 +1,25 @@
 // db.js
 const mongoose = require('mongoose');
-const config = require('./config');
+const config = require('./config/database');
 
-// Подключение к MongoDB
-mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// Функция для подключения к базе данных
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(config.mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        if (process.env.APP_ENV === 'development' && process.env.APP_DEBUG === 'true') {
+            console.log('Mongoose connected to', config.mongoURI);
+        } else {
+            console.log('Mongoose connected');
+        }
+    } catch (err) {
+        console.error('Mongoose connection error:', err);
+        throw err;
+    }
+}
 
 // Обработка событий подключения
 const db = mongoose.connection;
@@ -35,4 +48,6 @@ process.on('SIGINT', () => {
     });
 });
 
-//module.exports = db;
+module.exports = {
+    connectToDatabase
+};
