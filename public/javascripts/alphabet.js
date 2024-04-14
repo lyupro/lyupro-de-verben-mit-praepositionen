ec3001cd4bc5b9dceae8db82331666cf2ca477fc
Max Lyu <lyupro21@gmail.com>
2024-04-14 18:25:56 +0200

    ADDED: Functionality to open the /verbs page when clicking on the same letter of the alphabet
    Clicked on the same letter of the alphabet again, instead of a page with verbs for a specific letter, the /verbs page opened

--- public/javascripts/alphabet.js
+++ public/javascripts/alphabet.js
@@ -2,40 +2,77 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     const alphabetLinks = document.querySelectorAll('.alphabet a');
+    let currentLetter = null;
 
     function loadVerbsByLetter(event) {
         event.preventDefault();
         const letter = this.getAttribute('href').split('/').pop();
         const page = 1;
 
-        fetch(`/verbs/letter/${letter}/page/${page}`)
-            .then(response => response.text())
-            .then(html => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
 
-                const verbList = document.querySelector('.verbs');
-                const paginationContainer = document.querySelector('.pagination');
-                const titleElement = document.querySelector('title');
-                const headerElement = document.querySelector('h1');
+        if (letter === currentLetter) {
+            // Если нажата та же буква, загружаем страницу /verbs
+            fetch(`/verbs`)
+                .then(response => response.text())
+                .then(html => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(html, 'text/html');
 
-                if (verbList && paginationContainer && titleElement && headerElement) {
-                    verbList.innerHTML = doc.querySelector('.verbs').innerHTML;
-                    paginationContainer.innerHTML = doc.querySelector('.pagination').innerHTML;
-                    titleElement.textContent = doc.querySelector('title').textContent;
-                    headerElement.textContent = doc.querySelector('h1').textContent;
-                    history.pushState(null, null, `/verbs/letter/${letter}/page/${page}`);
+                    const verbList = document.querySelector('.verbs');
+                    const paginationContainer = document.querySelector('.pagination');
+                    const titleElement = document.querySelector('title');
+                    const headerElement = document.querySelector('h1');
 
-                    // Обновляем обработчики событий для пагинации
-                    const paginationLinks = paginationContainer.querySelectorAll('a');
-                    paginationLinks.forEach(link => {
-                        link.addEventListener('click', paginationClickHandler);
-                    });
-                } else {
-                    console.error('Elements .verbs or .pagination not found');
-                }
-            })
-            .catch(error => console.error('Error loading verbs:', error));
+                    if (verbList && paginationContainer && titleElement && headerElement) {
+                        verbList.innerHTML = doc.querySelector('.verbs').innerHTML;
+                        paginationContainer.innerHTML = doc.querySelector('.pagination').innerHTML;
+                        titleElement.textContent = doc.querySelector('title').textContent;
+                        headerElement.textContent = doc.querySelector('h1').textContent;
+                        history.pushState(null, null, `/verbs`);
+                        currentLetter = null;
+
+                        // Обновляем обработчики событий для пагинации
+                        const paginationLinks = paginationContainer.querySelectorAll('a');
+                        paginationLinks.forEach(link => {
+                            link.addEventListener('click', paginationClickHandler);
+                        });
+                    } else {
+                        console.error('One or more required elements not found');
+                    }
+                })
+                .catch(error => console.error('Error loading verbs:', error));
+        } else {
+            // Если нажата другая буква, загружаем страницу с глаголами по букве
+            fetch(`/verbs/letter/${letter}/page/${page}`)
+                .then(response => response.text())
+                .then(html => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(html, 'text/html');
+
+                    const verbList = document.querySelector('.verbs');
+                    const paginationContainer = document.querySelector('.pagination');
+                    const titleElement = document.querySelector('title');
+                    const headerElement = document.querySelector('h1');
+
+                    if (verbList && paginationContainer && titleElement && headerElement) {
+                        verbList.innerHTML = doc.querySelector('.verbs').innerHTML;
+                        paginationContainer.innerHTML = doc.querySelector('.pagination').innerHTML;
+                        titleElement.textContent = doc.querySelector('title').textContent;
+                        headerElement.textContent = doc.querySelector('h1').textContent;
+                        history.pushState(null, null, `/verbs/letter/${letter}/page/${page}`);
+                        currentLetter = letter;
+
+                        // Обновляем обработчики событий для пагинации
+                        const paginationLinks = paginationContainer.querySelectorAll('a');
+                        paginationLinks.forEach(link => {
+                            link.addEventListener('click', paginationClickHandler);
+                        });
+                    } else {
+                        console.error('Elements .verbs or .pagination not found');
+                    }
+                })
+                .catch(error => console.error('Error loading verbs:', error));
+        }
     }
 
     function paginationClickHandler(event) {
