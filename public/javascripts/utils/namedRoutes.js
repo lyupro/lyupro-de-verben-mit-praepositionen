da5cc4772c2505f702f57b9d4433089dd20ccd9e
Max Lyu <45335183+lyupro@users.noreply.github.com>
2024-06-19 20:51:51 +0200

    ADDED: At public\javascripts\utils\namedRoutes.js new front-end function fetchNamedRoute()

--- public/javascripts/utils/namedRoutes.js
+++ public/javascripts/utils/namedRoutes.js
@@ -1,7 +1,18 @@
 // public/javascripts/utils/namedRoutes.js
-async function fetchNamedRoute(name, params) {
-    const response = await fetch(`/api/named-routes?name=${name}&params=${JSON.stringify(params)}`);
+//window.fetchNamedRoute = async function(name, params = {}) {
+async function fetchNamedRoute(name, params = {}) {
+    console.log('public/javascripts/utils/namedRoutes.js | fetchNamedRoute() | name: ', name);
+    console.log('public/javascripts/utils/namedRoutes.js | fetchNamedRoute() | params: ', params);
+    const url = new URL('/api/named-routes', window.location.origin);
+    url.searchParams.append('name', name);
+    if (Object.keys(params).length > 0) {
+        url.searchParams.append('params', JSON.stringify(params));
+    }
+
+    const response = await fetch(url);
+    console.log('public/javascripts/utils/namedRoutes.js | fetchNamedRoute() | response: ', response);
     const data = await response.json();
+    console.log('public/javascripts/utils/namedRoutes.js | fetchNamedRoute() | data: ', data);
 
     if (response.ok) {
         return data.url;
